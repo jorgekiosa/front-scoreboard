4ce5b7ec72ee47f01addc02e5beca11a34c27803
Jorge Kiosa <jorge.kiosa@ucall.co.ao>
2025-01-02 17:02:43 +0100

    add new file vercel

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/',
-  build: {
-    outDir: 'dist'
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
