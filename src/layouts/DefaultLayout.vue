f6b39c431ea00209b2ae3aea2664e1e0fd0429d7
Jorge Kiosa <jorge.kiosa@ucall.co.ao>
2025-01-03 14:01:15 +0100

    file netlify

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -53,7 +53,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+    <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
             <img
@@ -235,6 +235,7 @@ watch(
     }
 );
 onMounted(()=>{
+  routeName.value=route.path
   islogado()
 })
 </script>
