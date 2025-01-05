1eaea43c4d9b2b7d6a57ff3aac924fa317ca44dc
Jorge Kiosa <jorge.kiosa@ucall.co.ao>
2025-01-06 00:37:36 +0100

    file netlify

--- src/components/broadcast.vue
+++ src/components/broadcast.vue
@@ -60,7 +60,7 @@
   const router = useRouter();
   const route = useRoute();
 
-  const socket = io('http://localhost:3007',{query: { code:route.query.code || '' }});
+  const socket = io(import.meta.env.VITE_WEBSOCKT_BASE_URL || 'http://localhost:3007',{query: { code:route.query.code || '' },transports: ['websocket','polling'],});
   
   // Definição de dados de exemplo para os times
   const timer = ref(0);
