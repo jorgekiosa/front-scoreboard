a4269f5b3b0f33f4b29af0d8e5c9814334574ffe
Jorge Kiosa <jorge.kiosa@ucall.co.ao>
2025-01-09 15:41:43 +0100

    adicionei a funcionalidade a actualizar o estado do scoreboard apos o termino do jogo

--- src/components/broadcast.vue
+++ src/components/broadcast.vue
@@ -15,7 +15,7 @@
         <div class="team p-0 text-dark">
           <div class="d-flex justify-content-between align-items-center">
             <div class="team d-flex border-top w-100" style="clip-path: polygon(0 0, 95% 0, 100% 100%, 0 100%);">
-              <span class="team-name fs-4 fw-bold" v-for="(part, index) in formatName(team1.name)" :key="index">
+              <span class="team-name fs-5 fw-bold" v-for="(part, index) in formatName(team1.name)" :key="index">
                 {{ part }}
                 <img
                   v-if="index !== formatName(team1.name).length - 1"
@@ -44,7 +44,7 @@
         <div class="team p-0 text-dark">
           <div class="d-flex justify-content-between align-items-center">
             <div class="team d-flex border-bottom w-100" style="clip-path: polygon(0 0, 95% 0, 100% 100%, 0 100%);">
-              <span class="team-name fs-4 fw-bold" v-for="(part, index) in formatName(team2.name)" :key="index">
+              <span class="team-name fs-5 fw-bold" v-for="(part, index) in formatName(team2.name)" :key="index">
                 {{ part }}
                 <img
                   v-if="index !== formatName(team2.name).length - 1"
